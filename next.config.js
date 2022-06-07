6cada335bf67d1b71a023840b4e082e617dd9075
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2022-06-08 00:30:04 +0200

    Add initial PWA support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 const nextBuildId = require("next-build-id");
+const withPWA = require("next-pwa");
 
-module.exports = {
+module.exports = withPWA({
     reactStrictMode: false,
     generateBuildId: async () => {
         const id = await nextBuildId({dir: __dirname});
         console.log('Generating build hash for NextJS modules:', id);
         return id;
+    },
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true
     }
-};
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, {Head, Html, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from "styled-components";
 
 class MyDocument extends Document {
@@ -28,6 +28,22 @@ class MyDocument extends Document {
             ],
         };
     }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="manifest" href="/manifest.json"/>
+                    <link rel="apple-touch-icon" href="/images/pwa/icon-256x256.png"></link>
+                    <meta name="theme-color" content="#fff"/>
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        );
+    };
 }
 
 export default MyDocument;
